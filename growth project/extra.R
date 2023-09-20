8cf7843b36df35545a1e51c19c248fd8b6355287
Lily Dun <89431483+lilydun1@users.noreply.github.com>
2023-09-21 08:30:33 +1000

    playing with RA

--- growth project/extra.R
+++ growth project/extra.R
@@ -4,8 +4,10 @@ GR_types_all = c("growth_stem_diameter", "GR_d","GR_d_age", "GR_d_50", "GR_d_25"
                  "growth_inv", "GR_w", "GR_w_age", "GR_w_50", "GR_w_25", "GR_w_75", 
                  "growth_leaf_area", "GR_la", "GR_la_age", "GR_la_50", "GR_la_25", "GR_la_75")
 GR_types_all = c("growth_stem_diameter", "growth_height", "growth_inv", "growth_leaf_area", 
+                 "GR_d_max", "GR_h_max", "GR_w_max", "GR_la_max")
+GR_types_all = c("growth_stem_diameter", "growth_height", "growth_inv", "growth_leaf_area", 
                  "GR_d_each_age", "GR_h_each_age", "GR_w_each_age", "GR_la_each_age")
-traits_Dgrowth_plots <- map(GR_types_all, ~plotting_Dgrowth(response = "LM_SM_slope_s", GR = .x))
+traits_Dgrowth_plots <- map(GR_types_all, ~plotting_Dgrowth(response = "LMA", GR = .x))
 ggarrange(plotlist = traits_Dgrowth_plots, common.legend = TRUE, nrow=2, ncol = 4)
 
 GR_types = c("GR_d_max")
@@ -26,16 +28,33 @@ d1 <- ggarrange(plotlist = traits_Dgrowth_plots, common.legend = TRUE, nrow=1)
 
 ggarrange(d1, d2, d3, nrow = 3)
 
-GR_against = c("growth_inv", "growth_leaf_area", "growth_height")
-traits_Dgrowth_plots <- map(GR_against, ~plotting_Dgrowth(response = "age", GR = .x))
+GR_against = c( "growth_stem_diameter", "growth_inv", "growth_leaf_area", "growth_height")
+traits_Dgrowth_plots <- map(GR_against, ~plotting_Dgrowth(response = "LMA", GR = .x))
 ggarrange(plotlist = traits_Dgrowth_plots, common.legend = TRUE)
 
 growth_data %>%
-  ggplot(aes(log10(GR_d_max), log10(growth_inv))) + 
+  mutate(RA_group = as.character(round(RA_max_1, 1))) %>% 
+  #filter(RA_max_1 == 0) %>% 
+  ggplot(aes(log10(LMA), log10(growth_stem_diameter), col = RA_group)) + 
   geom_point() +
-  geom_smooth(method ="lm", se = FALSE) +
+  geom_smooth(method = "lm", se = FALSE) +
   stat_poly_eq(use_label(c("eq", "R2", "P")),
                formula = formula1)
+
+df <- growth_data %>% 
+  filter(growth_leaf_area > -0.000001) %>% 
+  group_by(Species_name, age) %>% 
+  summarise(mean = mean(growth_leaf_area)) %>% 
+  ungroup() %>% 
+  group_by(age) %>% 
+  mutate(ranks = order(order(mean))) %>% 
+  filter(age != 32) %>% 
+  ggplot(aes(ranks, age, col = Species_name)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)+
+  stat_poly_eq(use_label(c("eq", "R2", "P")),
+               formula = formula1)
+  
 
 df <- growth_data %>% 
   filter(growth_leaf_area > -0.000001) %>% 
@@ -68,18 +87,18 @@ growth_data %>%
   stat_poly_eq(use_label(c("eq", "R2", "P")),
                formula = formula1)
 
+GR_types_all = c("growth_stem_diameter", "growth_height", "growth_inv", "growth_leaf_area")
+traits_Dgrowth_plots <- map(GR_types_all, ~plotting_Dgrowth(response = "age", GR = .x))
+ggarrange(plotlist = traits_Dgrowth_plots, common.legend = TRUE)
+
 growth_data %>% 
-  ggplot(aes(log10(GR_w), log10(growth_inv)))+
+  ggplot(aes((RA_max_1), log10(growth_inv), col = Species_name))+
   geom_point() +
   geom_smooth(method ="lm") +
   stat_poly_eq(use_label(c("eq", "R2", "P")),
                formula = formula1)
 
-ggpairs(growth_data, 
-        columns=, c("growth_stem_diameter", "growth_height", "growth_inv","growth_leaf_area"),
-        axisLabels='none',  
-        upper=list(continuous='blank')
-)
+
 
 df <- growth_data %>% 
   group_by(species) %>%   
